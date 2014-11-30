5441cf60670741d7cbec3448691ac6720764d9c8
Pedro Quintero Rojas <pqr@uh.cu>
2014-11-30 17:11:03 -0500

    reorganizing calculator

--- app/models/spree/calculator_package.rb
+++ app/models/spree/calculator_package.rb
@@ -1,7 +1,15 @@
 module Spree
-  class CalculatorPackage
+  class CalculatorPackage < BaseCalculator
 
-    def self.calculate_price(context, product)
+    def max_adults
+      1
+    end
+
+    def max_children
+      1
+    end
+
+    def calculate_price(context, product)
       return [product.price.to_f] if product.rates.empty?
       prices = []
       days = context.end_date.to_date - context.start_date.to_date rescue 1
@@ -9,40 +17,45 @@ module Spree
       product.rates.each do |r|
         next if context.start_date.present? && (context.start_date.to_date < r.start_date.to_date rescue false)
         next if context.end_date.present? && (context.end_date.to_date > r.end_date.to_date rescue false)
-        adults_array = self.get_adult_list(r, context.adult)
-        children_array = self.get_child_list(r, context.child)
+        adults_array = get_adult_list(r, context.adult)
+        children_array = get_child_list(r, context.child)
         combinations = adults_array.product(children_array)
         combinations.each do |ad, ch|
-          prices << self.get_rate_price(r, ad, ch) * days
+          prices << get_rate_price(r, ad, ch) * days
         end
       end
       prices
     end
 
+    def combination_string(rate)
+      ""
+    end
+
+    def get_rate_price(rate, adults, children)
+      adults = adults.to_i
+      children = children.to_i
+      price = adults * rate.one_adult.to_i + children * rate.one_child.to_i
+      price
+    end
+
     private
 
-        def self.get_adult_list(rate, pt_adults)
+    def get_adult_list(rate, pt_adults)
       if pt_adults.present?
         [pt_adults]
       else
-        [1, 2, 3]
+        (1..max_adults).to_a
       end
     end
 
-    def self.get_child_list(rate, pt_child)
+    def get_child_list(rate, pt_child)
       if pt_child.present?
         [pt_child]
       else
-        [1, 2]
+        (0..max_children).to_a
       end
     end
 
-    def self.get_rate_price(rate, adults, children)
-      adults = adults.to_i
-      children = children.to_i
-      price = adults * rate.one_adult + children * rate.one_child
-      price
-    end
 
   end
 end
