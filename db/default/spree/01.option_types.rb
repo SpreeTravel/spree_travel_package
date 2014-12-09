9bd0586037108d8bd69e0b31b8d4793600a11964
Pedro Quintero Rojas <pqr@uh.cu>
2014-12-09 07:49:07 -0500

    adding short field to seed data

--- db/default/spree/01.option_types.rb
+++ db/default/spree/01.option_types.rb
@@ -1,12 +1,12 @@
 ### This is the data
 option_types = [
-  {:name => "start_date", :presentation => "Start Date", :attr_type => 'date'},
-  {:name => "end_date", :presentation => "End Date", :attr_type => 'date'},
-  {:name => "adult", :presentation => "Adult", :attr_type => 'integer'},
-  {:name => "child", :presentation => "Child", :attr_type => 'integer'},
-  {:name => "one_child", :presentation => "One Child", :attr_type => 'float'},
-  {:name => "one_adult", :presentation => "One Adult", :attr_type => 'float'},
-  {:name => "destination", :presentation => "Destination", :attr_type => 'destination'},
+  {:name => "start_date", :presentation => "Start Date", :attr_type => 'date', :short => 'Start'},
+  {:name => "end_date", :presentation => "End Date", :attr_type => 'date', :short => 'End'},
+  {:name => "adult", :presentation => "Adult", :attr_type => 'integer', :short => 'Adult'},
+  {:name => "child", :presentation => "Child", :attr_type => 'integer', :short => 'Child'},
+  {:name => "one_child", :presentation => "One Child", :attr_type => 'float', :short => 'Adult'},
+  {:name => "one_adult", :presentation => "One Adult", :attr_type => 'float', :short => 'Child'},
+  {:name => "destination", :presentation => "Destination", :attr_type => 'destination', :short => 'Dest'},
 ]
 
 ### Creating Option Types
